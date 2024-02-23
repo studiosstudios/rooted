7e9cc4fe1126808ab8f0f8e8ce81dfed53536907
jffzhou <jffzhou@gmail.com>
2024-02-22 21:18:14 -0500

    colllision controller header

--- source/controllers/CollisionController.h
+++ source/controllers/CollisionController.h
@@ -8,57 +8,59 @@
 #ifndef CollisionController_h
 #define CollisionController_h
 
-#include <stdio.h>
+#include <cugl/cugl.h>
 #include <box2d/b2_world.h>
 #include <box2d/b2_contact.h>
 #include <box2d/b2_collision.h>
-//#include <box2d/level.h>
+#include "../objects/Level.h"
+
 
 class CollisionController {
 protected:
 //  MARK: - Properties
+    std::shared_ptr<Level> _level;
 
 public:
 
 //  MARK: - Constructors
 
-//    /**
-//     * Constructs a Collision Controller
-//     */
-//    CollisionController() {};
-//
-//    /**
-//     * Destructs a Collision Controller
-//     */
-//    ~CollisionController() { dispose(); }
-//
-//    /**
-//     * Disposes of all resources in this instance of Collision Controller
-//     */
-//    void dispose() { _game = nullptr; }
-//
-//    /**
-//     * Initializes a Collision Controller
-//     */
-//    bool init(const std::shared_ptr<GameModel> game);
-//
-//
-////  MARK: - Callbacks
-//
-//    /**
-//     * Callback for when two obstacles in the world begin colliding
-//     */
-//    void beginContact(b2Contact* contact);
-//
-//    /**
-//     * Callback for when two obstacles in the world end colliding
-//     */
-//    void endContact(b2Contact* contact);
-//
-//    /**
-//     * Callback for determining if two obstacles in the world should collide.
-//     */
-//    bool shouldCollide(b2Fixture* f1, b2Fixture* f2);
+    /**
+     * Constructs a Collision Controller
+     */
+    CollisionController() {};
+
+    /**
+     * Destructs a Collision Controller
+     */
+    ~CollisionController() { dispose(); }
+
+    /**
+     * Disposes of all resources in this instance of Collision Controller
+     */
+    void dispose() { _level = nullptr; }
+
+    /**
+     * Initializes a Collision Controller
+     */
+    bool init(const std::shared_ptr<Level> level);
+
+
+//  MARK: - Callbacks
+
+    /**
+     * Callback for when two obstacles in the world begin colliding
+     */
+    void beginContact(b2Contact* contact);
+
+    /**
+     * Callback for when two obstacles in the world end colliding
+     */
+    void endContact(b2Contact* contact);
+
+    /**
+     * Callback for determining if two obstacles in the world should collide.
+     */
+    bool shouldCollide(b2Fixture* f1, b2Fixture* f2);
 
 };
 

616a88cd39877ab7a13249411453cf73590c23e0
jffzhou <jffzhou@gmail.com>
2024-02-22 22:23:24 -0500

    map

--- source/objects/Map.h
+++ source/objects/Map.h
@@ -5,9 +5,32 @@
 #ifndef ROOTED_MAP_H
 #define ROOTED_MAP_H
 
+#include "BabyCarrot.h"
+#include "Carrot.h"
+#include "Farmer.h"
+#include "Wheat.h"
 
 class Map {
+private:
+    std::vector<std::shared_ptr<BabyCarrot>> _babies;
 
+    std::vector<std::shared_ptr<Carrot>> _carrots;
+    
+    std::vector<std::shared_ptr<Farmer>> _farmers;
+    
+    std::vector<std::shared_ptr<Wheat>> _wheat;
+    
+    
+public:
+    
+    Map() {};
+    
+    ~Map() {};
+
+    bool init();
+    
+    static std::shared_ptr<Map> alloc();
+    
 };
 
 

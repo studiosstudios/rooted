496ccd1955991396a1341a0a2120243885ee4e4c
Joshua Guo <72211490+joshuakguo@users.noreply.github.com>
2024-03-02 15:11:51 -0500

    Update AIController.cpp

--- source/controllers/AIController.cpp
+++ source/controllers/AIController.cpp
@@ -4,6 +4,7 @@
 
 #include "AIController.h"
 #include "../objects/BabyCarrot.h"
+#include "time.h"
 
 #define RADIUS 0.2f
 #define MOVEMENT_SPEED 0.75f
@@ -12,33 +13,27 @@ using namespace cugl;
 
 bool AIController::init(std::shared_ptr<Map> &map) {
     _map = map;
+    srand(time(NULL)^2);
     return true;
 }
 
 void AIController::updateBabyCarrot(const std::shared_ptr<BabyCarrot> &babyCarrot) {
     if (nearTarget(babyCarrot->getPosition(), babyCarrot->getTarget())) {
         babyCarrot->setTarget(Vec2::ZERO);
+        babyCarrot->setMovement(Vec2(0,0));
+        babyCarrot->applyForce();
     }
     if (babyCarrot->hasTarget()) {
-        Vec2 movement = Vec2(babyCarrot->getPosition(), babyCarrot->getTarget());
-        movement.normalize();
+        Vec2 movement = Vec2(babyCarrot->getPosition(), babyCarrot->getTarget()).normalize();
         movement * MOVEMENT_SPEED;
         babyCarrot->setMovement(Vec2(0,0));
         babyCarrot->setMovement(movement);
         babyCarrot->applyForce();
     } else {
-        Vec2 newTarget = Vec2(((float) std::rand()/ RAND_MAX)*10+10, ((float) std::rand()/ RAND_MAX)*5+10);
+        Vec2 newTarget = Vec2(((float) std::rand()/ RAND_MAX)*10+1, ((float) std::rand()/ RAND_MAX)*5+5);
         babyCarrot->setTarget(newTarget);
         
     }
-//    if (babyCarrot->getPosition().x >= 20) {
-//        babyCarrot->setMovement(Vec2(0,0));
-//        babyCarrot->setMovement(Vec2(-1,0));
-//    }
-//    else if (babyCarrot->getX() <= 10) {
-//        babyCarrot->setMovement(Vec2(0,0));
-//        babyCarrot->setMovement(Vec2(1,0));
-//    }
 }
 
 bool AIController::nearTarget(const Vec2 vec1, const Vec2 vec2) {

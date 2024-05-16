4c79258c8d60dcdce97b46d3d40c00bfb5038e4f
Nicole Thean <nicole@thean.com>
2024-05-16 17:20:29 -0400

    new maps without decs

--- assets/level-editor/maps.tsx
+++ assets/level-editor/maps.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="maps" tilewidth="640" tileheight="360" tilecount="9" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="maps" tilewidth="640" tileheight="360" tilecount="27" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="Map">
   <properties>
@@ -26,41 +26,166 @@
    <property name="blade_color_scale" type="float" value="10"/>
    <property name="name" value="unit_1"/>
   </properties>
-  <image width="640" height="360" source="../textures/maps/unit_1.png"/>
+  <image width="640" height="360" source="../../../../Downloads/Untitled - April 17, 2024 18.34.39 4.png"/>
  </tile>
  <tile id="4" type="Map">
   <properties>
    <property name="blade_color_scale" type="float" value="10"/>
    <property name="name" value="unit_2"/>
   </properties>
-  <image width="640" height="360" source="../textures/maps/unit_2.png"/>
+  <image width="640" height="360" source="../../../../Downloads/Untitled - April 17, 2024 18.34.39 5.png"/>
  </tile>
  <tile id="6" type="Map">
   <properties>
    <property name="blade_color_scale" type="float" value="10"/>
    <property name="name" value="unit_4"/>
   </properties>
-  <image width="640" height="360" source="../textures/maps/unit_4.png"/>
+  <image width="640" height="360" source="../../../../Downloads/Untitled - April 17, 2024 18.34.39 6.png"/>
  </tile>
  <tile id="7" type="Map">
   <properties>
    <property name="blade_color_scale" type="float" value="10"/>
    <property name="name" value="unit_5"/>
   </properties>
-  <image width="640" height="360" source="../textures/maps/unit_5.png"/>
+  <image width="640" height="360" source="../../../../Downloads/Untitled - April 17, 2024 18.34.39 7.png"/>
  </tile>
  <tile id="8" type="Map">
   <properties>
    <property name="blade_color_scale" type="float" value="10"/>
    <property name="name" value="unit_6"/>
   </properties>
-  <image width="640" height="360" source="../textures/maps/unit_6.png"/>
+  <image width="640" height="360" source="../../../../Downloads/Untitled - April 17, 2024 18.34.39 8.png"/>
  </tile>
  <tile id="9" type="Map">
   <properties>
    <property name="blade_color_scale" type="float" value="10"/>
    <property name="name" value="unit_3"/>
   </properties>
-  <image width="640" height="360" source="../textures/maps/unit_3.png"/>
+  <image width="640" height="360" source="../../../../Downloads/Untitled - April 17, 2024 18.34.39 9.png"/>
+ </tile>
+ <tile id="10" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_tl_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_tl_1.png"/>
+ </tile>
+ <tile id="11" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_t_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_t_1.png"/>
+ </tile>
+ <tile id="12" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_tr_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_tr_1.png"/>
+ </tile>
+ <tile id="28" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_l_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_l_1.png"/>
+ </tile>
+ <tile id="14" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_m_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_m_1.png"/>
+ </tile>
+ <tile id="15" type="Map">
+  <properties>
+   <property name="name" value="unit_r_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_r_1.png"/>
+ </tile>
+ <tile id="16" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_bl_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_bl_1.png"/>
+ </tile>
+ <tile id="17" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_b_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_b_1.png"/>
+ </tile>
+ <tile id="18" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_br_1"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_br_1.png"/>
+ </tile>
+ <tile id="29" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_tl_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_tl_2.png"/>
+ </tile>
+ <tile id="20" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_t_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_t_2.png"/>
+ </tile>
+ <tile id="21" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_tr_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_tr_2.png"/>
+ </tile>
+ <tile id="22" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_l_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_l_2.png"/>
+ </tile>
+ <tile id="23" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_m_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_m_2.png"/>
+ </tile>
+ <tile id="30" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_r_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_r_2.png"/>
+ </tile>
+ <tile id="25" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_bl_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_bl_2.png"/>
+ </tile>
+ <tile id="26" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_b_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_b_2.png"/>
+ </tile>
+ <tile id="27" type="Map">
+  <properties>
+   <property name="blade_color_scale" type="float" value="10"/>
+   <property name="name" value="unit_br_2"/>
+  </properties>
+  <image width="640" height="360" source="../textures/maps/unit_br_2.png"/>
  </tile>
 </tileset>

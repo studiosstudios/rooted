a1d0fcf7ae45bdbeb52a195c5417063242515f0a
Nicole Thean <nicole@thean.com>
2024-05-17 03:40:09 -0400

    add test fence but weird scaling

--- assets/level-editor/environment.tsx
+++ assets/level-editor/environment.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="environment" tilewidth="2204" tileheight="2007" tilecount="6" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="environment" tilewidth="2204" tileheight="2048" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="PlantingSpot">
   <image width="96" height="96" source="rootingSpotTemp.png"/>
@@ -38,5 +38,69 @@
    <property name="name" value="mill"/>
   </properties>
   <image width="1620" height="2007" source="../textures/mill_single.png"/>
+ </tile>
+ <tile id="14" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence0"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence0.png"/>
+ </tile>
+ <tile id="8" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence1"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence7.png"/>
+ </tile>
+ <tile id="12" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence2"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence2.png"/>
+ </tile>
+ <tile id="11" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence3"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence3.png"/>
+ </tile>
+ <tile id="10" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence5"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence5.png"/>
+ </tile>
+ <tile id="9" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence6"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence6.png"/>
+ </tile>
+ <tile id="13" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence7"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence1.png"/>
+ </tile>
+ <tile id="7" type="Decoration">
+  <properties>
+   <property name="frame_cols" type="int" value="1"/>
+   <property name="frame_rows" type="int" value="1"/>
+   <property name="name" value="fence8"/>
+  </properties>
+  <image width="2048" height="2048" source="../../../../Downloads/ezgif-2-033c57843d-png-2048x2048-sprite-png/fence8.png"/>
  </tile>
 </tileset>
